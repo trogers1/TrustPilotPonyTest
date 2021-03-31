e1a82077f0347e5840a745a04b702c68b1c181fc
Taylor Rogers <taylor.rea.rogers@gmail.com>
2021-03-31 19:19:16 +0100

    PonySaver: notes, loadMaze, new errors, setMazeData, print

--- src/PonySaver/PonySaver.ts
+++ src/PonySaver/PonySaver.ts
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
-import { readableToString } from '../utils';
+import { readableToString, colors } from '../utils';
+
+const { RedFg, RedFg_Bright } = colors;
 
 export type WallPosition = 'west' | 'north';
 export type MazePosition = WallPosition[];
@@ -12,7 +14,18 @@ export type MazeResponse = {
   data: MazePosition[];
 };
 
-export class PonySaver {
+/* Handle:
+- Game no longer active
+- No routes found
+  - None exist
+  - domukun blocks all paths
+  - Maybe head in the direction
+- Panic mode (run away from domokun)
+  - determine opposite direction
+  - prioritize paths going the opposite direction
+*/
+
+export default class PonySaver {
   playerName: string;
   mazeHeight: number;
   mazeWidth: number;
@@ -22,17 +35,25 @@ export class PonySaver {
   domokunPosition!: number;
   endPoint!: number;
   maze!: MazePosition[];
+  autoPrint: boolean;
 
   constructor(
     playerName: string,
     mazeHeight: number,
     mazeWidth: number,
-    difficulty: number
+    difficulty: number,
+    autoPrint = false
   ) {
     this.playerName = playerName;
     this.mazeHeight = mazeHeight;
     this.mazeWidth = mazeWidth;
     this.difficulty = difficulty;
+    this.autoPrint = autoPrint;
+  }
+
+  async loadMaze(mazeId: string) {
+    this.mazeId = mazeId;
+    await this.getMazeData();
   }
 
   async createMaze() {
@@ -51,11 +72,25 @@ export class PonySaver {
     if (!response.ok) {
       const body = await readableToString(response.body);
 
-      throw new Error(`Unsuccessful request to create maze. Result:\n${body}`);
+      throw new Error(
+        RedFg(
+          `Unsuccessful request to create maze. Result:\n${RedFg_Bright(body)}`
+        )
+      );
     }
     const json: Record<string, string> = await response.json();
 
     this.mazeId = json.maze_id;
+  }
+
+  setMazeData(data: MazeResponse) {
+    this.ponyPosition = data.pony[0];
+    this.domokunPosition = data.domokun[0];
+    this.endPoint = data['end-point'][0];
+    this.maze = data.data;
+    this.mazeWidth = data.size[0];
+    this.mazeHeight = data.size[1];
+    this.difficulty = data.difficulty;
   }
 
   async getMazeData() {
@@ -66,13 +101,35 @@ export class PonySaver {
     if (!response.ok) {
       const body = await readableToString(response.body);
 
-      throw new Error(`Unsuccessful request to GET maze. Result:\n${body}`);
+      throw new Error(
+        RedFg(
+          `Unsuccessful request to GET maze. Result:\n${RedFg_Bright(body)}`
+        )
+      );
     }
     const json: MazeResponse = await response.json();
 
-    this.ponyPosition = json.pony[0];
-    this.domokunPosition = json.domokun[0];
-    this.endPoint = json['end-point'][0];
-    this.maze = json.data;
+    return json;
+  }
+
+  findPaths() {}
+
+  async print() {
+    let response = await fetch(
+      `https://ponychallenge.trustpilot.com/pony-challenge/maze/${this.mazeId}/print`,
+      { method: 'GET' }
+    );
+
+    const body = await readableToString(response.body);
+    if (!response.ok) {
+      throw new Error(
+        RedFg(
+          `Unsuccessful request to print the maze. Result:\n${RedFg_Bright(
+            body
+          )}`
+        )
+      );
+    }
+    console.log(JSON.stringify(body));
   }
 }
