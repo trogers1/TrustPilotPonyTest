a41fc780181026bcfef3094cbf7fa7a5b767793b
Taylor Rogers <taylor.rea.rogers@gmail.com>
2021-04-02 10:25:51 +0100

    Move PonySaver types to their own file

---
+++ src/PonySaver/types.ts
@@ -0,0 +1,25 @@
+export type WallPosition = 'west' | 'north';
+export type MazePosition = WallPosition[];
+export type MazeResponse = {
+  pony: number[];
+  domokun: number[];
+  'end-point': number[];
+  size: number[];
+  difficulty: number;
+  data: MazePosition[];
+  maze_id: 'string';
+  'game-state': {
+    state: string;
+    'state-result': string;
+  };
+};
+export type CoordinatePair = {
+  x: number;
+  y: number;
+};
+export type ValidDirections = {
+  north: boolean;
+  south: boolean;
+  east: boolean;
+  west: boolean;
+};
