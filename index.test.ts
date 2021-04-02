0c440e44b96678cf880168c0200fd07e61886d1d
Taylor Rogers <taylor.rea.rogers@gmail.com>
2021-04-02 21:50:35 +0100

    Cleanup tests

--- index.test.ts
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import { main } from './index';
-
-// describe('Main', () => {
-//   it('Should return true', () => {
-//     assert.strictEqual(main(), true);
-//   });
-// });
